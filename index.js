b1d6ff9b7b820acda2e7393d1604ba3a80f81cd8
Amy Jackson <aj81623@gmail.com>
2017-10-10 20:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 if (name === "Susan"){
     console.log("The name is correct")
@@ -9,4 +9,3 @@ else {
 // Don't worry about this
 module.exports = { name, height
 }
-
